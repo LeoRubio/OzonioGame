7419f4a977a1711480dbbd6767566010779d2b85
leonardofontourarubio@gmail.com <leonardofontourarubio@gmail.com>
2015-06-01 15:52:00 -0300

    moleculas colision

--- OzonioGame/OzonioGame/OGCarbon.swift
+++ OzonioGame/OzonioGame/OGCarbon.swift
@@ -11,28 +11,35 @@ import SpriteKit
 
 class OGCarbon : OGMolecule {
     
+
+    
     init(){
-        
+
         super.init(imageNamed: "BlackCircle")
         
+        self.lifeMax = 1
+        
         self.name = "CARBONMOLECULE"
-        
-        self.size.width *= 0.5
-        self.size.height *= 0.5
-        
+        self.size.width *= 0.8
+        self.size.height *= 0.8
         self.physicsBody = SKPhysicsBody (circleOfRadius: self.size.width / 1.5)
-  
+        self.physicsBody?.dynamic = true
+        
+
     }
     
+    
     override func update() {
+        
         var childXPosition = self.position.x + self.parent!.position.x
         var childYPosition = self.position.y + self.parent!.position.y
         //self.physicsBody!.applyImpulse(CGVectorMake(1.0, 1.0))
-        self.physicsBody!.applyForce(CGVectorMake(0.4, 1.0))
+        self.physicsBody!.applyForce(CGVectorMake(0.2, 6.0))
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
     
 }
