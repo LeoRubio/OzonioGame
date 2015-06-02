7cbe07e48947139b646fa7344de2fd28a35479d9
LeAlbus <jpedro_lopes21@hotmail.com>
2015-06-02 13:57:52 -0300

    Criando classe do tempo
    classe que vai fazer as transições temporais

---
+++ OzonioGame/OzonioGame/OGTimeCount.swift
@@ -0,0 +1,14 @@
+//
+//  OGTimeCount.swift
+//  OzonioGame
+//
+//  Created by Pedro Lopes on 6/2/15.
+//  Copyright (c) 2015 FantasyName. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class OGTimeCount: SKSpriteNode {
+   
+}
